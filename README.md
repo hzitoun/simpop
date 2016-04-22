cdd38132d224dfc2bfd7d0b7293f93cd0192ce1b
Hamed ZITOUN <hzitoun@enseirb-matmeca.fr>
2016-04-22 19:12:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# simple-popup-html5
+
+# How to use it ? 
+
+- Add the following HTML 5 component to your page to initialize the popup
+
+  ```html
+  <popup 
+      id="popupid" 
+	  text="popup body comes here" 
+	  title="Popup Title" 
+	  onconfirm="fnOnConfirm();" 
+	  oncancel="var msg = 'canceled!'; alert(msg);" 
+	  height="120" 
+	  width="400" 
+	  resizable="true"
+      class = "css class"
+	  >
+</popup>
+```
+
+  \
+
+- Include the JS script (just before closing the body tag)
+  ```html
+  <script src="js/lib/simple-popup.min.js"></script>
+   ```
+  
+- Open it
+   ```html
+  <button onclick="popupid.open();"></button>
+```
+
+
+
+
+
+
+# Configs
+
+* `id`: DOM id used to open the popup (required)
+* `title`:  title of the popup (optional)
+* `text`:  the body of the popup (optional)
+* `onconfirm`: JS code to be executed when the confirm button is clicked (optional)
+* `oncancel`: JS code to be executed when the cancel button is clicked (optional)
+* `confirmbtn`:  label of confirm button (default is 'Yes') (optional)
+* `canelbtn`: label of the cancel button (default is 'No') (optional)
+* `height`: height (optional)
+* `width`: width (optional)
+* `resizable : true/false (optional)
+
+
+Notice that [jquery ui](https://jqueryui.com/) is required!
+
+
+
+
---
+++ js/lib/simple-popup.js
@@ -0,0 +1,45 @@
+if ('undefined' == typeof window.jQuery) {
+  throw new Error('jQuery is required!');
+} else if ('undefined' == typeof window.jQuery.ui) {
+  throw new Error('jQuery ui is required!');
+}
+$('popup').each(function () {
+  var attributes = $(this).context.attributes;
+  $(this).html(attributes.text ? attributes.text.value : '');
+  var confirmbtn = attributes.confirmbtn ? attributes.confirmbtn.value : 'Yes';
+  var cancelbtn = attributes.cancelbtn ? attributes.cancelbtn.value : 'No';
+  var height = attributes.height ? attributes.height.value : 120;
+  var width = attributes.width ? attributes.width.value : 120;
+  var popupClass =  attributes.class ? attributes.class.value : '';
+  var resizable = attributes.resizable ? attributes.resizable.value === 'true' : true
+  $(this).dialog({
+    dialogClass: popupClass,
+    height: height,
+    width: width,
+    autoOpen: false,
+    resizable: resizable,
+    buttons: [
+      {
+        text: confirmbtn,
+        click: function () {
+          if (attributes.onconfirm) {
+            (Function(attributes.onconfirm.value)) ()
+          }
+          $(this).dialog('close');
+        }
+      },
+      {
+        text: cancelbtn,
+        click: function () {
+          if (attributes.oncancel) {
+            (Function(attributes.oncancel.value)) ()
+          }
+          $(this).dialog('close');
+        }
+      }
+    ]
+  });
+eval(attributes.id.value).open = function(){
+ 	$(this).dialog('open');
+ } 
+});
